0767e4fed6130d540fa5a1e2b6eb2f6d013d86e3
AntoineSmagghe <44842796+AntoineSmagghe@users.noreply.github.com>
2021-05-18 16:30:46 +0000

    Deploying to gh-pages from master @ 1668d4b01d364c2956429a340926027d5e8ace31 🚀

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-(()=>{"use strict";WA.openPopup("popupRectangle","This is an imporant message!",[{label:"Got it!",className:"primary",callback:a=>{}}]),WA.sendChatMessage("Hello world","Mr Robot")})();
+(()=>{"use strict";WA.openPopup("popupRectangle","This is an imporant message!",[{label:"Got it!",className:"primary",callback:a=>{}}])})();
 //# sourceMappingURL=script.js.map
