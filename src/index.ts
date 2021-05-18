1668d4b01d364c2956429a340926027d5e8ace31
Antoine Smagghe <smagghe@proximumgroup.com>
2021-05-18 18:30:03 +0200

    Change tiles, upgrade meeting room

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 /// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
-
 // You can write your WorkAdventure script here, if any.
 // The "WA" global object is available from anywhere.
-
-// console.log('Script started successfully');
 
 WA.openPopup('popupRectangle', 'This is an imporant message!', [
   {
@@ -14,6 +11,6 @@ WA.openPopup('popupRectangle', 'This is an imporant message!', [
     },
   },
 ]);
-WA.sendChatMessage('Hello world', 'Mr Robot');
 
+// WA.sendChatMessage('Hello world', 'Mr Robot');
 // WA.openCoWebSite('https://workadventu.re');
